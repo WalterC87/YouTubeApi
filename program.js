6c79df649837fe001ddaf49f58a8db9d26bdc6b1
Walter Cordero <wdlara.8789@gmail.com>
2013-07-22 14:02:10 -0600

    agregar la app

---
+++ program.js
@@ -0,0 +1,93 @@
+var Models = {
+	Video: Backbone.Model.extend(),
+};
+
+var Collections = {
+	Videos: Backbone.Collection.extend({
+		model: Models.Video,
+		url: 'http://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&max-results=12',
+		parse: function(resp) {
+			//console.log('VideosCollection: Recibo del Servidor y hago un parsing de la data');
+			return resp.data.items;
+		},
+	}),
+};
+
+
+var Views = {
+	SingleVideo: Backbone.View.extend({
+		className: 'video',
+		initialize: function() {
+			this.template = _.template($('#single-video-template').val());
+		},
+		render: function() {
+			//console.log('SingleVideo: render entero');
+			this.$el.html(this.template({video:this.model.toJSON()}));
+			//console.log('SingleVideo: render');
+			return this;
+		},
+	}),
+
+
+
+	VideosApp: Backbone.View.extend({
+		initialize: function() {
+			_.bindAll(this);
+			this.template = _.template($('#app-template').val());
+			this.searchBox = new Views.SearchBox();
+			this.searchBox.on('searchRequest', this.performSearch, this);
+			this.collection = new Collections.Videos();
+			this.collection.on('reset', this.showVideos, this);
+			this.performSearch();
+		},
+		render: function() {
+			//console.log('VideosApp: render entero');
+			this.$el.html(this.template());
+			this.$el.find('#video-search-box').html(this.searchBox.render().el);
+			this.showVideos();
+			//console.log('VideosApp: leaving render');
+			return this;
+		},
+		showVideos: function() {
+			this.$el.find('#video-list-container').empty();
+			var v = null;
+			this.collection.each(function(item, idx) {
+				v = new Views.SingleVideo({model:item});
+				this.$el.find('#video-list-container').append(v.render().el);
+			}, this);
+			return this;
+		},
+		performSearch: function(evdata) {
+			evdata = evdata || {};
+			//console.log('VideosApp: entering performSearch - queryString: ' + evdata.queryString);
+			this.collection.fetch({data:{q:evdata.queryString}});
+			//console.log('VideosApp: leaving performSearch');
+		},
+	}),
+
+	SearchBox: Backbone.View.extend({
+		events: {
+			'click #search-submit': 'performSearch',
+		},
+		initialize: function() {
+			this.template = _.template($('#search-box-template').val())
+		},
+		render: function() {
+			console.log('SearchBox: entering render');
+			this.$el.html(this.template());
+			console.log('SearchBox: leaving render');
+			return this;
+		},
+		performSearch: function() {
+			//console.log('SearchBox: entering performSearch');
+			queryString = this.$el.find('#search-query').val();
+			this.trigger('searchRequest', {queryString:queryString});
+			//console.log('SearchBox: leaving performSearch');
+		},
+	}),
+};
+
+$(document).ready(function() {
+	var vs = new Views.VideosApp();
+	vs.setElement($('#container')).render();
+});
